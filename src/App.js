6e15a799d775387cbc67f0d4712957e963890684
Marina8989 <marina.mill209@gmail.com>
2021-07-14 20:23:52 -0700

    react quote generator

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import data from './data.js';
+import './index.css';
 
 function App() {
     const [quote, setQuote] = useState(data[0]);
     const [count, setCount] = useState(0);
+    const reference = useRef(null);
 
     const handleClick = () => {
         let randNum = Math.floor(Math.random() * data.length);
         setCount(randNum);
         setQuote(data[count]);
+        console.log(reference.current);
         console.log(quote);
         console.log(count);
+        console.log(reference.current)
+        reference.current.style.background = 'pink';
     }
+    useEffect(() => {
+        // reference.current.style.background = 'pink';
+    })
     
     return (
-        <div>
-             <div key={quote.id}>
-                <h2>{quote.quote}</h2>
-                <h6>{quote.name}</h6>
+         <main className="container" ref={reference}>
+             <div key={quote.id} className="quoteContainer">
+                <h2 className="quote">{quote.quote}</h2>
+                <h6 className="author">{quote.name}</h6>
+                <button className="btn" type="button" onClick={handleClick}>New quote</button>
             </div>
-            <button type="button" onClick={handleClick}>New quote</button>
-        </div>
+         </main>
     )
-}
+} 
 
 export default App
